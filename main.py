c12f17308b4fe100b68d5c6124cd1f34d86506d5
SatyamSSJ10 <60954779+SatyamSSJ10@users.noreply.github.com>
2023-05-25 22:03:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ image = Image.open('Screenshot 2023-05-25 131902.jpg')
 draw = ImageDraw.Draw(image)
 
 # Set a minimum acceptable font size
-min_font_size = 10
+min_font_size = 12
+line_spacing_multiplier = 1
 
 def split_into_lines(text, font, draw, max_width):
     words = text.split()
@@ -30,7 +31,7 @@ with open('text_boxes.txt', 'r') as f:
         
         # Extract text and bounding box
         text, bbox = text_list[0], text_list[1]
-
+        draw.rectangle(bbox, fill="white")
         if len(text.strip()) == 0:
             # If the text is empty or only consists of spaces, skip this line
             continue
@@ -46,12 +47,16 @@ with open('text_boxes.txt', 'r') as f:
             max_width = bbox[2] - bbox[0]
             lines = split_into_lines(text, font, draw, max_width)
 
-        # Adjust the bounding box height for each line of text
-        line_height = (bbox[3] - bbox[1]) // len(lines)
+        # Calculate the total text height and the line height
+        line_height = draw.textsize('Ay', font=font)[1]
+        total_text_height = line_height * len(lines) * line_spacing_multiplier
+
+        # Calculate the y-coordinate of the first line to center the text vertically
+        start_y = bbox[1] + (bbox[3] - bbox[1] - total_text_height) / 2
 
         # Draw the bounding boxes and text
         for i, line in enumerate(lines):
-            line_bbox = [bbox[0], bbox[1] + i * line_height, bbox[2], bbox[1] + (i + 1) * line_height]
+            line_bbox = [bbox[0], start_y + i * line_height, bbox[2], start_y + (i + 1) * line_height]
             draw.rectangle(line_bbox, fill="white")
 
             # Calculate the centered text position
